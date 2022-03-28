fa4c52ec4f13a5efefb49440ee3d430bcf35aefd
taslimamindia <mamadoutaslima.diallo@usmba.ac.ma>
2022-03-28 16:14:32 +0000

    Update RoleRepository.java

--- src/main/java/ma/fst/tkhzn/sdsi/repositories/RoleRepository.java
+++ src/main/java/ma/fst/tkhzn/sdsi/repositories/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ma.fst.tkhzn.sdsi.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
-
+    // Changement
 }
